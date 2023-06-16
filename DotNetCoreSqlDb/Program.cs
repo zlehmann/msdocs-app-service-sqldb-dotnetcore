316043dd21fb55787ecec59d3f51278cc5ad9a7a
Zachary Lehmann <zlehmann@gmail.com>
2023-06-15 20:35:45 -0400

    uncommenting mydatabase context build

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -4,8 +4,8 @@ using DotNetCoreSqlDb.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
-//builder.Services.AddDbContext<MyDatabaseContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+builder.Services.AddDbContext<MyDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
 
 builder.Services.AddStackExchangeRedisCache(options =>
 {
